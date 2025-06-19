46724a41d5222731cb7fd6b33f98fedd1db473e8
sachin150597 <142768725+sachin150597@users.noreply.github.com>
2025-06-19 07:30:03 +0000

    trivy

---
+++ sonar-java-demo/src/main/java/com/example/Calculator.java
@@ -0,0 +1,12 @@
+// src/main/java/com/example/Calculator.java
+package com.example;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
